0a420caafa4a3d9c9aaaf07d56734cfdb4f79110
[sa] <[m1m22a17@st.oit.ac.jp]>
2022-07-04 11:45:48 +0900

    monsterList修正

--- app/src/main/java/se22/MonsterList.java
+++ app/src/main/java/se22/MonsterList.java
@@ -1,11 +1,11 @@
 package se22;
 
 public class MonsterList {
-  Monster monsters[] = new Monster[5];
+  private Monster monsters[] = new Monster[5];
 
   public Monster[] generetMonsters() {
     Monster mons;
-    for(int i=0,i<5;i++){
+    for (int i = 0; i < 5; i++) {
       mons = new Monster(i, i);
       monsters[i] = mons;
     }
